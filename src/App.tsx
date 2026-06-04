ac6943f6f75ffc6929e0d8c6446fb0a0c6383433
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-04 12:52:21 +0600

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click</Button>
     </div>
   );
 }
